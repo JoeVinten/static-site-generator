0528aa8acfb5392e6157fb89e151e29bd8015567
Joe Vinten <joseph.vinten@gmail.com>
2024-04-02 19:58:03 +0100

    style: update styling

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,23 +2,28 @@ import unittest
 
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
+
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_node(self):
-        node = HTMLNode("a", "A Link", None, {"href": "www.google.com", "target": "_blank"})
+        node = HTMLNode(
+            "a", "A Link", None, {"href": "www.google.com", "target": "_blank"}
+        )
         props_to_html_return = node.props_to_html()
         expected_output = ' href="www.google.com" target="_blank"'
         self.assertEqual(props_to_html_return, expected_output)
-    
+
     def test_to_html_renders(self):
         leaf_node = LeafNode("p", "This is a paragraph of text.")
-        self.assertEqual(leaf_node.to_html(), '<p>This is a paragraph of text.</p>')
+        self.assertEqual(leaf_node.to_html(), "<p>This is a paragraph of text.</p>")
         leaf_node_2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-        self.assertEqual(leaf_node_2.to_html(), '<a href="https://www.google.com">Click me!</a>')
-    
+        self.assertEqual(
+            leaf_node_2.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
     def test_to_html(self):
-         node = LeafNode(None, 'Just a String')
-         self.assertEqual(node.to_html(), 'Just a String')
-    
+        node = LeafNode(None, "Just a String")
+        self.assertEqual(node.to_html(), "Just a String")
+
     def test_to_html_children(self):
         child_node = LeafNode("p", "child node")
         node = ParentNode("div", [child_node])
@@ -31,18 +36,19 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(parent_node.to_html(), "<div><p><b>bold node</b></p></div>")
 
     def test_to_html_many_children(self):
-        children = ParentNode("p", [
-            LeafNode("b", "bold"),
-            LeafNode("a", "link", {"href": "https://www.google.com"}),
-            LeafNode(None, "no tag")
-        ])
+        children = ParentNode(
+            "p",
+            [
+                LeafNode("b", "bold"),
+                LeafNode("a", "link", {"href": "https://www.google.com"}),
+                LeafNode(None, "no tag"),
+            ],
+        )
         self.assertEqual(
-            children.to_html(), 
-                '<p><b>bold</b><a href="https://www.google.com">link</a>no tag</p>'
-            )
-
-        
+            children.to_html(),
+            '<p><b>bold</b><a href="https://www.google.com">link</a>no tag</p>',
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
